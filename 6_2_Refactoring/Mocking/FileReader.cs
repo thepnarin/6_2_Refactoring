d3fe7456ac6038bcf7866c205bacfad6a7f1276a
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 23:32:43 +0700

    Refactoring

---
+++ 6_2_Refactoring/Mocking/FileReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_2_Refactoring.Mocking
+{
+    public interface IFileReader
+    {
+        string Read(string path);
+    }
+
+    public class FileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
